843ffd200b800d85a9126d37b9528f9ec1b8b4bc
carrot03 <113475602+carrot03@users.noreply.github.com>
2022-10-27 13:21:44 -0400

    Update ClientRegisterPage.java
    I created the private instances for the textviews & textedits
    I created the listeners to the textedits instances
    DELETED private Button donebutton INSTEAD I created it as a textView

--- Mealer/app/src/main/java/com/example/mealer/ClientRegisterPage.java
+++ Mealer/app/src/main/java/com/example/mealer/ClientRegisterPage.java
@@ -7,21 +7,47 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class ClientRegisterPage extends AppCompatActivity {
-    private Button donebutton;
+//added implememntation
 
+public class ClientRegisterPage extends AppCompatActivity implements View.OnClickListener {
+    //*************************************************************************************************
+    //deleted the instance and created it after as a textView instance
+    //private Button donebutton;
+    //*************************************************************************************************
+    
+    //*************************************************************************************************
+    //added the instances for the edit texters
+    private EditText editTextname, editTextLastName, editTextinputEmail, editTextpassword, editTextaddress, editTextcreditCard, editTextCVV, editTextExpiry;
+    //added the instances for the textview
+    //donebutton is the same one as for the old button above (deleted)
+    private TextView creditText, donebutton;
+    //*************************************************************************************************
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_client_register_page);
 
         donebutton = (Button) findViewById(R.id.donebutton);
+        
         donebutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 openMainActivity();
             }
         });
+        
+        //*********************************************************************************************
+        //added the view access to the edit texts 
+        editTextname = (EditText) findViewById(R.id.inputname);
+        editTextLastName = (EditText) findViewById(R.id.inputlastname);
+        editTextinputEmail = (EditText) findViewById(R.id.inputemail);
+        editTextpassword = (EditText) findViewById(R.id.inputpass);
+        editTextaddress = (EditText) findViewById(R.id.address);
+        editTextcreditCard = (EditText) findViewById(R.id.creditcard);
+        editTextCVV = (EditText) findViewById(R.id.cvv);
+        editTextExpiry = (EditText) findViewById(R.id.expiry);
+        //*********************************************************************************************
     }
     //this takes you back to login
     //this is a test comment 
