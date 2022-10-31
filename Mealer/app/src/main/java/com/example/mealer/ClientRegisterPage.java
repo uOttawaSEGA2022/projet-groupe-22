2b2a307ca27399ca22c240d9b7ffbdd321d7e794
carrot03 <113475602+carrot03@users.noreply.github.com>
2022-10-31 11:01:27 -0400

    Update ClientRegisterPage.java

--- Mealer/app/src/main/java/com/example/mealer/ClientRegisterPage.java
+++ Mealer/app/src/main/java/com/example/mealer/ClientRegisterPage.java
@@ -3,46 +3,32 @@ package com.example.mealer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.location.Address;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 //added implememntation
 
 public class ClientRegisterPage extends AppCompatActivity implements View.OnClickListener {
-    //*************************************************************************************************
-    //deleted the instance and created it after as a textView instance
-    //private Button donebutton;
-    //*************************************************************************************************
-    
-    //*************************************************************************************************
-    //added the instances for the edit texters
+
     private EditText editTextname, editTextLastName, editTextinputEmail, editTextpassword, editTextaddress, editTextcreditCard, editTextCVV, editTextExpiry;
-    //added the instances for the textview
-    //donebutton is the same one as for the old button above (deleted)
+    //private FirebaseAuth fAuth;
+
     private TextView creditText, donebutton;
-    //*************************************************************************************************
-    
-    @Override
+
+        @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_client_register_page);
 
         donebutton = (Button) findViewById(R.id.donebutton);
-        //added the listener method
+
         donebutton.setOnClickListener(this);
-        //deleted the method and re-created it again!
-        /*
-        donebutton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openMainActivity();
-            }
-        });
-        */
-        
-        //*********************************************************************************************
-        //added the view access to the edit texts 
+
         editTextname = (EditText) findViewById(R.id.inputname);
         editTextLastName = (EditText) findViewById(R.id.inputlastname);
         editTextinputEmail = (EditText) findViewById(R.id.inputemail);
@@ -51,13 +37,15 @@ public class ClientRegisterPage extends AppCompatActivity implements View.OnClic
         editTextcreditCard = (EditText) findViewById(R.id.creditcard);
         editTextCVV = (EditText) findViewById(R.id.cvv);
         editTextExpiry = (EditText) findViewById(R.id.expiry);
-        //*********************************************************************************************
-        
-    }
-    
+            /*once we add firebase
+            fAuth = FirebaseAuth.getInstance();
+        fDataRef = FirebaseDatabase.getInstance().getReference("UserData");
+             */
+        }
+
     private void registerUser(){
         //Creating the getters for the inputs
-        
+
         String inputname=editTextname.getText().toString().trim();
         String inputlastname=editTextLastName.getText().toString().trim();
         String inputemail=editTextinputEmail.getText().toString().trim();
@@ -66,9 +54,9 @@ public class ClientRegisterPage extends AppCompatActivity implements View.OnClic
         String creditcard=editTextcreditCard.getText().toString().trim();
         String cvv=editTextCVV.getText().toString().trim();
         String expiry=editTextExpiry.getText().toString().trim();
-        
-        //Creating the gerror messages
-        
+
+        //Creating the error messages
+
         if(inputname.isEmpty()){
             editTextname.setError("First name is required");
             editTextname.requestFocus();
@@ -84,31 +72,31 @@ public class ClientRegisterPage extends AppCompatActivity implements View.OnClic
             editTextinputEmail.requestFocus();
             return;
         }
-        
+
         if (!Patterns.EMAIL_ADDRESS.matcher(inputemail).matches()){
             editTextinputEmail.setError("Please provide a valid email");
             editTextinputEmail.requestFocus();
             return;
         }
-        
+
         if(inputpass.isEmpty()){
             editTextpassword.setError("Password is required");
             editTextpassword.requestFocus();
             return;
         }
-        
+
         if (inputpass.length()<6){
             editTextpassword.setError("Email is required");
             editTextpassword.requestFocus();
             return;
         }
-        
+
         if(address.isEmpty()){
             editTextaddress.setError("Address is required");
             editTextaddress.requestFocus();
             return;
         }
-        
+
         if(creditcard.isEmpty()){
             editTextcreditCard.setError("Credit card is required");
             editTextcreditCard.requestFocus();
@@ -124,16 +112,34 @@ public class ClientRegisterPage extends AppCompatActivity implements View.OnClic
             editTextCVV.requestFocus();
             return;
         }
-        
+
         if(expiry.isEmpty()){
             editTextExpiry.setError("The expiry date is required");
             editTextExpiry.requestFocus();
             return;
         }
-        
+
     }
+    /*
+    public void onComplete(@NonNull Task<AuthResult> task){
+                if (task.isSuccessful())
+                {
+                    String id = fAuth.getCurrentUser().getUid();
+                        fDataRef.child(id).setValue(new UserData(inputName, userRole, id, inputEmail));
+                    Toast.makeText(Register.this,"User Created",Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(Register.this, Login.class);
+                    startActivity(intent);
+                    finish();
+                    }
+                else
+                {
+                    Toast.makeText(Register.this,"Error! "+task.getException().getMessage(),Toast.LENGTH_SHORT).show();
+                }
+    }
+     */
+
     //this takes you back to login
-    //this is a test comment 
+    //this is a test comment
     public void openMainActivity(){
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
