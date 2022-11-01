2a0a04cc7d31b2f5c785c645b33e858f70262d1e
carrot03 <113475602+carrot03@users.noreply.github.com>
2022-11-01 09:22:30 -0400

    Update ClientRegisterPage.java

--- Mealer/app/src/main/java/com/example/mealer/ClientRegisterPage.java
+++ Mealer/app/src/main/java/com/example/mealer/ClientRegisterPage.java
@@ -1,5 +1,6 @@
 package com.example.mealer;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -11,24 +12,32 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
 
 //added implememntation
 
-public class ClientRegisterPage extends AppCompatActivity implements View.OnClickListener {
+public class ClientRegisterPage extends AppCompatActivity {
 
     private EditText editTextname, editTextLastName, editTextinputEmail, editTextpassword, editTextcreditCard, editTextCVV, editTextExpiry;
-    //private FirebaseAuth fAuth;
+    private FirebaseAuth fAuth;
 
     private TextView creditText, donebutton;
 
-        @Override
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_client_register_page);
 
         donebutton = (Button) findViewById(R.id.donebutton);
 
-        donebutton.setOnClickListener(this);
+        donebutton.setOnClickListener((View.OnClickListener) this);
 
         editTextname = (EditText) findViewById(R.id.inputname);
         editTextLastName = (EditText) findViewById(R.id.inputlastname);
@@ -37,99 +46,114 @@ public class ClientRegisterPage extends AppCompatActivity implements View.OnClic
         editTextcreditCard = (EditText) findViewById(R.id.creditcard);
         editTextCVV = (EditText) findViewById(R.id.cvv);
         editTextExpiry = (EditText) findViewById(R.id.expiry);
-            /*once we add firebase
-            fAuth = FirebaseAuth.getInstance();
-        fDataRef = FirebaseDatabase.getInstance().getReference("UserData");
-             */
-        }
 
-    private void onRegisterButtonClicked(View view){
+        fAuth = FirebaseAuth.getInstance();
+
+    }
+
+    private void onRegisterButtonClicked(View view) {
         //Creating the getters for the inputs
 
-        String inputname=editTextname.getText().toString().trim();
-        String inputlastname=editTextLastName.getText().toString().trim();
-        String inputemail=editTextinputEmail.getText().toString().trim();
-        String inputpass=editTextpassword.getText().toString().trim();
-        String creditcard=editTextcreditCard.getText().toString().trim();
-        String cvv=editTextCVV.getText().toString().trim();
-        String expiry=editTextExpiry.getText().toString().trim();
+        String inputname = editTextname.getText().toString().trim();
+        String inputlastname = editTextLastName.getText().toString().trim();
+        String inputemail = editTextinputEmail.getText().toString().trim();
+        String inputpass = editTextpassword.getText().toString().trim();
+        String creditcard = editTextcreditCard.getText().toString().trim();
+        String cvv = editTextCVV.getText().toString().trim();
+        String expiry = editTextExpiry.getText().toString().trim();
 
         //Creating the error messages
 
-        if(inputname.isEmpty()){
+        if (inputname.isEmpty()) {
             editTextname.setError("First name is required");
             editTextname.requestFocus();
             return;
         }
-        if(inputlastname.isEmpty()){
+        if (inputlastname.isEmpty()) {
             editTextLastName.setError("Last name is required");
             editTextLastName.requestFocus();
             return;
         }
-        if(inputemail.isEmpty()){
+        if (inputemail.isEmpty()) {
             editTextinputEmail.setError("Email is required");
             editTextinputEmail.requestFocus();
             return;
         }
 
-        if (!Patterns.EMAIL_ADDRESS.matcher(inputemail).matches()){
+        if (!Patterns.EMAIL_ADDRESS.matcher(inputemail).matches()) {
             editTextinputEmail.setError("Please provide a valid email");
             editTextinputEmail.requestFocus();
             return;
         }
 
-        if(inputpass.isEmpty()){
+        if (inputpass.isEmpty()) {
             editTextpassword.setError("Password is required");
             editTextpassword.requestFocus();
             return;
         }
 
-        if (inputpass.length()<6){
+        if (inputpass.length() < 6) {
             editTextpassword.setError("Email is required");
             editTextpassword.requestFocus();
             return;
         }
 
-        if(creditcard.isEmpty()){
+        if (creditcard.isEmpty()) {
             editTextcreditCard.setError("Credit card is required");
             editTextcreditCard.requestFocus();
             return;
         }
-        if(cvv.isEmpty()){
+        if (cvv.isEmpty()) {
             editTextCVV.setError("cvv is required");
             editTextCVV.requestFocus();
             return;
         }
-        if (cvv.length()<3){
+        if (cvv.length() < 3) {
             editTextCVV.setError("The number should not be less than 3 numbers");
             editTextCVV.requestFocus();
             return;
         }
 
-        if(expiry.isEmpty()){
+        if (expiry.isEmpty()) {
             editTextExpiry.setError("The expiry date is required");
             editTextExpiry.requestFocus();
             return;
         }
 
-    }
-    /*
-    public void onComplete(@NonNull Task<AuthResult> task){
-                if (task.isSuccessful())
-                {
-                    String id = fAuth.getCurrentUser().getUid();
-                        fDataRef.child(id).setValue(new UserData(inputName, userRole, id, inputEmail));
-                    Toast.makeText(Register.this,"User Created",Toast.LENGTH_SHORT).show();
-                    Intent intent = new Intent(Register.this, Login.class);
-                    startActivity(intent);
-                    finish();
+
+        final Address address;
+
+
+        fAuth.createUserWithEmailAndPassword(inputemail, inputpass)
+                        .
+
+                addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {   //creating the user object
+                            User user = new User(inputemail, inputpass);
+
+                            FirebaseDatabase.getInstance().getReference("users")
+                                    .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                    .setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+
+                                        public void onComplete(Task<Void> task) {
+                                            if (task.isSuccessful()) {
+                                                Toast.makeText(ClientRegisterPage.this, "User has been successful", Toast.LENGTH_LONG).show();
+                                            } else {
+                                                Toast.makeText(ClientRegisterPage.this, "User has not been successful. Try again!", Toast.LENGTH_LONG).show();
+                                            }
+
+                                        }
+                                    });
+                        } else {
+                            Toast.makeText(ClientRegisterPage.this, "User has not been successful. Try again!", Toast.LENGTH_LONG).show();
+                        }
                     }
-                else
-                {
-                    Toast.makeText(Register.this,"Error! "+task.getException().getMessage(),Toast.LENGTH_SHORT).show();
-                }
+                });
     }
-     */
+
+
 
     //this takes you back to login
     //this is a test comment
@@ -138,5 +162,5 @@ public class ClientRegisterPage extends AppCompatActivity implements View.OnClic
         startActivity(intent);
     }
 
-}
+
 }
