4bf0e8c9808ff87e712e96ecaaafa617630a1700
bAnalya <105563153+bAnalya@users.noreply.github.com>
2022-11-18 23:26:53 -0500

    Update Meal.java
    Changes

--- Mealer/app/src/main/java/com/example/mealer/Meal.java
+++ Mealer/app/src/main/java/com/example/mealer/Meal.java
@@ -1,3 +1,57 @@
 public class Meal {
 	
+	private String chef ;
+	private String mealName;
+	private String mealType;
+	private String gastronomyType;
+	private double price;
+	
+	
+	public Meal (String chef,String mealName,String mealType,String gastronomyType,double price){
+		this.chef = chef ;
+		this.mealName = mealName ;
+		this.mealType = mealType ;
+		this.gastronomyType = gastronomyType ;
+		this.price = price ;		
+	}
+	
+	//Setters
+	public void setChef (String chef){
+		this.chef = chef ;
+	}
+	
+	public void setMealName(String mealName){
+		this.mealName = mealName ;
+	}
+	
+	public void setMealType(String mealType){
+		this.mealType = mealType ;
+	}
+	public void setGastronomyType(String gastronomyType){
+		this.gastronomyType = gastronomyType ;
+	}
+	public void setPrice(double price){
+		this.price = price ;	
+	}
+	
+	//Getters
+	public String getChef(){
+		return this.chef;
+	}
+	public String getMealName(){
+		return this.mealName;
+	}
+	public String getMealType(){
+		return this.mealType;
+	}
+	public String getGastronomyType(){
+		return this.gastronomyType;
+	}
+	public double getPrice(){
+		return this.price;
+	}
+	
+
+	
+	
 }
