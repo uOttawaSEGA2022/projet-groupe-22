f1ec77370eedf788328d627146c5a8f74b04bfda
mostefaiii <mostefaimeriem@gmail.com>
2022-11-18 22:09:08 -0500

    greets the user on login :]

--- Mealer/app/src/main/java/com/example/mealer/PageMain.java
+++ Mealer/app/src/main/java/com/example/mealer/PageMain.java
@@ -1,5 +1,6 @@
 package com.example.mealer;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -7,8 +8,11 @@ import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class PageMain extends AppCompatActivity {
     FirebaseUser fUser;
@@ -27,10 +31,17 @@ public class PageMain extends AppCompatActivity {
         fUser = fAuth.getCurrentUser();
         String IDstring = fUser.getUid();
 
-        String name = String.valueOf(reference.child("users").child(IDstring).child("name").get());
-        System.out.println(name);
+        reference = FirebaseDatabase.getInstance().getReference().child("users").child(IDstring).child("name");
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String name = snapshot.getValue(String.class);
+                Toast.makeText(PageMain.this,"Welcome, "+ name +"!",Toast.LENGTH_LONG).show();
+            }
 
-        Toast.makeText(PageMain.this,"Welcome,"+ name +"!",Toast.LENGTH_SHORT).show();
-
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
+        });
     }
 }
