e8ce1fe0d12af7424a37fe72dd041f9df87bdf5d
carrot03 <113475602+carrot03@users.noreply.github.com>
2022-11-28 16:16:21 -0500

    Update Complaint.java

--- Mealer/app/src/main/java/com/example/mealer/Complaint.java
+++ Mealer/app/src/main/java/com/example/mealer/Complaint.java
@@ -20,13 +20,13 @@ public class Complaint {
 
     //Setters
     public void setClientUid(String clientUid){this.clientUid = clientUid;}
-    public void setChef(String chef){ this.chefUid = chef;}
+    public void setChefUid(String chef){ this.chefUid = chef;}
     public void setDate(String date){ this.date = date;}
     public void setComplaintText(String complaintText){this.complaintText = complaintText;}
 
     //getters
     public String getClientUid(){return this.clientUid;}
-    public String getChef(){return this.chefUid;}
+    public String getChefUid(){return this.chefUid;}
     public String getDate(){return this.date;}
     public String getComplaintText(){return this.complaintText;}
 }
