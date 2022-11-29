c9860640e9f973eac3e06391bbdcb80d2bdc5a79
mostefaiii <mostefaimeriem@gmail.com>
2022-11-29 00:25:46 -0500

    some discussion

--- Mealer/app/src/main/java/com/example/mealer/Administrator.java
+++ Mealer/app/src/main/java/com/example/mealer/Administrator.java
@@ -1,8 +1,13 @@
 package com.example.mealer;
 
+//TODO: how i think this should work:
+//TODO: admin class should have a reference to the database
+
 public class Administrator extends User{
     //  TODO: this is a work in progresss
     public Administrator(){
         super.setRole("Admin");
     }
+
+
 }
