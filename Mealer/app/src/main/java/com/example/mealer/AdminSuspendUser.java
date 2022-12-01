408e5b7a75b81faf638f32ac5773b44352db736e
carrot03 <113475602+carrot03@users.noreply.github.com>
2022-12-01 00:22:21 -0500

    Create AdminSuspendUser.java
    class for the admin to set the susp details

---
+++ Mealer/app/src/main/java/com/example/mealer/AdminSuspendUser.java
@@ -0,0 +1,64 @@
+package com.example.mealer;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.CalendarView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class AdminSuspendUser extends AppCompatActivity {
+
+    DatabaseReference  chefReference;
+    Boolean status;
+
+    FirebaseAuth fAuth;
+    FirebaseUser fUser;
+
+    CalendarView calendarView;
+    TextView suspDate;
+    Calendar calendar;
+    SimpleDateFormat dateFormat;
+    String date;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_suspend_user);
+
+        fAuth = FirebaseAuth.getInstance();
+        fUser = fAuth.getCurrentUser();
+
+        calendarView = (CalendarView) findViewById(R.id.calendarView);
+        suspDate = (TextView) findViewById(R.id.suspDate);
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView calendarView, int i, int i1, int i2) {
+                //Date format : MM/DD/YYYY
+                String date = (i1 + 1) + "/" + i2 + "/" + 1;
+                suspDate.setText(date);
+
+
+                //chefReference = Administrator.getSuspChefRef();
+            }
+        });
+    }
+//TODO take the chef id from the admin class
+
+    private void suspendTemp (String chefID , String time){
+            //chefID.setInitialtime(time);
+    }
+
+    private void suspendDef (String chefID){
+            //chefID.changeStatus();
+    }
+}
