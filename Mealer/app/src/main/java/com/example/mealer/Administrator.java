7998c1ee81249711c4f64c9a3814786d1a007b60
carrot03 <113475602+carrot03@users.noreply.github.com>
2022-12-01 00:18:40 -0500

    Update Administrator.java
    added 2 methods of getting the reference of the chef to be suspended

--- Mealer/app/src/main/java/com/example/mealer/Administrator.java
+++ Mealer/app/src/main/java/com/example/mealer/Administrator.java
@@ -1,11 +1,29 @@
 package com.example.mealer;
 
+//TODO: how i think this should work:
+//TODO: admin class should have a reference to the database
+//response : that is right. this is how it should work
+
+import com.google.firebase.database.DatabaseReference;
 
 public class Administrator extends User{
+
+    static DatabaseReference chefSuspended;
+
     //  TODO: this is a work in progresss
     public Administrator(){
         super.setRole("Admin");
     }
+    //TODO idea of using it to save the reference of the chef
+/*
+    public void saveSuspChefRef(DatabaseReference chefSuspended){
+        this.chefSuspended = chefSuspended;
+    }
 
+    public static DatabaseReference getSuspChefRef(){
+        return chefSuspended;
+    }
+
+ */
 
 }
