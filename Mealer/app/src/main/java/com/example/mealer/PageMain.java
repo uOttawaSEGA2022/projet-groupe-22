756f6c0da552fb5b9a5996597d44f2a54a0f71d9
carrot03 <113475602+carrot03@users.noreply.github.com>
2022-12-05 23:26:11 -0500

    Update PageMain.java

--- Mealer/app/src/main/java/com/example/mealer/PageMain.java
+++ Mealer/app/src/main/java/com/example/mealer/PageMain.java
@@ -1,9 +1,15 @@
 package com.example.mealer;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -20,12 +26,82 @@ public class PageMain extends AppCompatActivity {
     FirebaseAuth fAuth;
     FirebaseDatabase database = FirebaseDatabase.getInstance();
     DatabaseReference reference = database.getReference();
+    Button addComplaintBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page_main);
+
+        addComplaintBtn = findViewById(R.id.addComplaintBtn);
+        addComplaintBtn.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {addComplaint();}
+        });
     }
+
+    public void addComplaint(){
+
+            AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+            LayoutInflater inflater = getLayoutInflater();
+            final View dialogView = inflater.inflate(R.layout.add_complaint, null);
+            dialogBuilder.setView(dialogView);
+
+            final EditText ComplaintChefName = (EditText) dialogView.findViewById(R.id.ComplaintChefName);
+            final EditText complaintDate  = (EditText) dialogView.findViewById(R.id.complaintDate);
+            final EditText complaintText  = (EditText) dialogView.findViewById(R.id.complaintText);
+            final Button addComplaintBtn = (Button) dialogView.findViewById(R.id.addComplaintBtn);
+
+            dialogBuilder.setTitle("Add complaint");
+            final AlertDialog b = dialogBuilder.create();
+            b.show();
+
+                addComplaintBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {{
+
+                    String chefName = ComplaintChefName.getText().toString().trim();
+                    String date = complaintDate.getText().toString().trim();
+                    String text = complaintText.getText().toString().trim();
+
+                    DatabaseReference complaintReference = FirebaseDatabase.getInstance().getReference().child("complaints");
+
+                    //checking if the value is provided
+                    if (!(TextUtils.isEmpty(chefName) & TextUtils.isEmpty(date) & TextUtils.isEmpty(text))) {
+
+                        //getting a unique id using push().getKey() method
+                        //it will create a unique id and we will use it as the Primary key for our Complaint
+                        String id = reference.push().getKey();
+
+                        //creating a Product Object
+                        Complaint complaint = new Complaint(id, chefName, date, text);
+
+                        //saving the product
+                        complaintReference.child(id).setValue(complaint);
+
+                        //setting edittext to blank again
+                        ComplaintChefName.setText("");
+                        complaintDate.setText("");
+                        complaintText.setText("");
+
+                        //displaying a success toast
+                        successToaster();
+                        b.dismiss();
+                    } else{
+                        //if the values are not given displaying a toast
+                        failingToaster();
+                        b.dismiss();
+
+                    }
+                }}
+            });
+    }
+
+    public void successToaster(){Toast.makeText(this, "Complaint added", Toast.LENGTH_LONG).show();}
+    public void failingToaster(){Toast.makeText(this, "Please fill all with the needed info", Toast.LENGTH_LONG).show();}
+
+
     public void onStart() {
         super.onStart();
         fAuth = FirebaseAuth.getInstance();
@@ -45,4 +121,5 @@ public class PageMain extends AppCompatActivity {
             }
         });
     }
+
 }
