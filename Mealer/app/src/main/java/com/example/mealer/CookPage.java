3c08fe80acbb8279850384eafe0b6a2701414220
carrot03 <113475602+carrot03@users.noreply.github.com>
2022-12-06 17:06:21 -0500

    Update CookPage.java

--- Mealer/app/src/main/java/com/example/mealer/CookPage.java
+++ Mealer/app/src/main/java/com/example/mealer/CookPage.java
@@ -155,6 +155,7 @@ public class CookPage extends AppCompatActivity {
         final Button buttonDeleteMeal = (Button) dialogView.findViewById(R.id.deleteMealBtn);
         final Button buttonDisplayMeal = (Button) dialogView.findViewById(R.id.displayMealBtn);
 
+        dialogBuilder.setTitle("What would you like to do with this meal?");
         final AlertDialog b = dialogBuilder.create();
         b.show();
         
