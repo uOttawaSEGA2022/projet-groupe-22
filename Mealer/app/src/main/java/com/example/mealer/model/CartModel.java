2541df63e9ece53747fdd924111fc952b32326b5
carrot03 <113475602+carrot03@users.noreply.github.com>
2022-12-09 15:55:52 -0500

    small changes

--- Mealer/app/src/main/java/com/example/mealer/model/CartModel.java
+++ Mealer/app/src/main/java/com/example/mealer/model/CartModel.java
@@ -2,10 +2,9 @@ package com.example.mealer.model;
 
 
 public class CartModel  {
-    private String key, name, chefName, price;
+    private String key, name, chefUid, price;
     private  int quantity;
     private float totalPrice;
-    //status is set as false when the meal is added to the cart
     private boolean status ;
 
     public CartModel() {
@@ -14,15 +13,16 @@ public class CartModel  {
 
     public void setKey(String key) {this.key = key;}
     public void setName(String name) {this.name = name;}
-    public void setChefName(String chefName) {this.chefName = chefName;}
+    public void setChefName(String chefUid) {this.chefUid = chefUid;}
     public void setPrice(String price) {this.price = price;}
     public  void setQuantity(int quantity) {this.quantity = quantity;}
     public void setTotalPrice(float totalPrice) {this.totalPrice = totalPrice;}
     public void setStatus(boolean status) {this.status = status;}
 
+
     public String getKey() {return key;}
     public String getName() {return name;}
-    public String getChefName() {return chefName;}
+    public String getChefName() {return chefUid;}
     public String getPrice() {return price;}
     public int getQuantity() {return quantity;}
     public float getTotalPrice() {return totalPrice;}
